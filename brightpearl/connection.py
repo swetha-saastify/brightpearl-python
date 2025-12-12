5b8a150003e440dfe1ce2f8df5234615686d4662
swetha-saastify <swetha@saastify.ai>
2025-12-12 22:44:19 +0530

    Update connection.py

--- brightpearl/connection.py
+++ brightpearl/connection.py
@@ -14,7 +14,7 @@ class OauthConnection(object):
         self.client_secret = client_secret
         self._session.headers = {
             "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Content - Type": "application/x-www-form-urlencoded"
         }
 
     def make_request(self, url, method, data=None):
